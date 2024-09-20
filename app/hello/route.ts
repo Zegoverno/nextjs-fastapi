f5b393b5a9f0ecf949ab008b6495df69ccc53369
zegoverno <zegoverno@hotmail.com>
2024-09-20 15:38:50 -0300

    teste

--- app/hello/route.ts
+++ app/hello/route.ts
@@ -1,5 +1,7 @@
+import { NextResponse } from 'next/server';
+
 export const dynamic = 'force-static'
- 
+
 export async function GET() {
-  return Response.json({ hello: 'world' })
+  return NextResponse.json({ hello: 'world' })
 }
